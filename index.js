329e20139f4e191f6a18709f5cd5ac58829d0cb3
yatinburhmi <yatinburhmi@gmail.com>
2022-07-24 22:34:34 -0400

    Cake restockinggit add .

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const CAKE_ORDERED = 'CAKE_ORDERED'
+const redux = require('redux');
+const createStore = redux.createStore;
 
+const CAKE_ORDERED = 'CAKE_ORDERED'
+const CAKE_RESTOCKED = 'CAKE_RESTOCKED'
 
 //action creator - a function that returns the object. 
 function orderCake(){
@@ -9,10 +12,16 @@ function orderCake(){
     }
 }
 
+function restocked(qty){
+    return{
+        type: CAKE_RESTOCKED,
+        payload: qty,
+    }
+}
+
  
 const initialState = {
     numOfCakes: 10,
-    anotherProperty: 0,
 }
 
 // reducer = pure function taking previous state and action as param
@@ -23,8 +32,24 @@ const reducer = (state = initialState, action) => {
                 ...state,
                 numOfCakes: state.numOfCakes - 1
             }
+        case CAKE_RESTOCKED:
+            return{
+                ...state,
+                numOfCakes: state.numOfCakes + action.payload
+            }
         default:
             return state
     }
 }
 
+const store = createStore(reducer)
+console.log('Initial State ', store.getState());
+
+const unsubscribe = store.subscribe(() => console.log('Updated State ', store.getState()));
+
+//we throw in the action creator method in dispatch function so that if in future action is changed, we can simply change the action creator instead of going in all the files. 
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(restocked(3))
+unsubscribe();
